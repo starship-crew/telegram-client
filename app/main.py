dfbda7a014496373c5275ed7050b52fc8889be7c
Mark Meliksetyan <markmelix@gmail.com>
2023-04-23 08:29:50 +0300

    remove store handlers registering while they're not implemented

--- app/main.py
+++ app/main.py
@@ -1,16 +1,15 @@
 from config import PATH_TO_DB
 from data.db_session import global_init
-from create_bot import  dp
+from create_bot import dp
 from services import db
 from handlers import (
-        start, 
-        help_, 
-        ship, 
-        store, 
-        fight, 
-        garage, 
-        wallet, 
-    )
+    start,
+    help_,
+    ship,
+    fight,
+    garage,
+    wallet,
+)
 
 import logging
 from aiogram import executor
@@ -26,16 +25,14 @@ from aiogram import types
 def main():
     global_init(PATH_TO_DB)
 
-
     start.register_handlers_start(dp)
     help_.register_handlers_help(dp)
-    store.register_handlers_store(dp)
     fight.register_handlers_fight(dp)
     garage.register_handlers_garage(dp)
     ship.register_handlers_ship(dp)
     wallet.register_handlers_wallet(dp)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
     executor.start_polling(dp, skip_updates=True)
