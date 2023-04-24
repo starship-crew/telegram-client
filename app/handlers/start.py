ee70896522afd283aaac1a3b727261eae9ba9acb
Mark Meliksetyan <markmelix@gmail.com>
2023-04-24 12:32:55 +0300

    fix typo

--- app/handlers/start.py
+++ app/handlers/start.py
@@ -1,5 +1,5 @@
 from template import render_template
-from services import api 
+from services import api
 from services import db
 from .help_ import cmd_help
 from keyboard import kb
@@ -14,32 +14,33 @@ class NewCerw(StatesGroup):
     crew_name = State()
 
 
-#@dp.message_handler(commands=['start'], state=None)
+# @dp.message_handler(commands=['start'], state=None)
 async def cmd_start(message: types.Message):
     user_id = message.from_id
     if db.get_api_token(user_id):
-        await message.reply("Извини но у тебя уже есть экипаж.\
- Одному пользователю доступно созоать лишь <b>один</b> экипаж.",
-                parse_mode="HTML", 
-                            reply_markup=kb)
+        await message.reply(
+            "Извини но у тебя уже есть экипаж.\
+ Одному пользователю можно иметь только <b>один</b> экипаж.",
+            parse_mode="HTML",
+            reply_markup=kb,
+        )
     else:
         await NewCerw.crew_name.set()
-        await message.answer(render_template("start.j2"), 
-                             parse_mode="HTML")
+        await message.answer(render_template("start.j2"), parse_mode="HTML")
 
 
-#@dp.message_handler(state=NewCerw.crew_name)
+# @dp.message_handler(state=NewCerw.crew_name)
 async def get_crew_name(message: types.Message, state: FSMContext):
     crew_name, user_id = message.text, message.from_id
 
     # verificatin
-    response = api.create_crew(crew_name) 
-    try :
+    response = api.create_crew(crew_name)
+    try:
         if response["message"]:
             return await message.reply(render_template("name_error.j2"))
     except Exception:
         pass
-    
+
     # save api token
     API_TOKEN = response["token"]
     db.save_new_user(user_id, API_TOKEN)
@@ -49,14 +50,15 @@ async def get_crew_name(message: types.Message, state: FSMContext):
 
     await state.finish()
     content = {
-            "crew_name": crew_name, 
-            }
+        "crew_name": crew_name,
+    }
 
     # send message
-    await message.answer(render_template("sucsess_start.j2", content), 
-                         parse_mode="HTML", 
-                         reply_markup=kb)
+    await message.answer(
+        render_template("sucsess_start.j2", content), parse_mode="HTML", reply_markup=kb
+    )
     await cmd_help(message)
+
 
 def register_handlers_start(dp: Dispatcher):
     dp.register_message_handler(cmd_start, commands=["start"], state=None)
